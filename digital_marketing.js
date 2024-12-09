4b462018367715520222cf81d2eb467eb44ee082
prabha34 <142577975+prabha34@users.noreply.github.com>
2024-12-09 20:35:17 +0530

    commit

---
+++ digital_marketing.js
@@ -0,0 +1,8 @@
+// const dropdownTitle = document.querySelector(".dropdown-title");
+// const dropdownContent = document.querySelector(".dropdown-content");
+
+// dropdownTitle.addEventListener("click", () => {
+//   const isVisible = dropdownContent.style.display === "block";
+//   dropdownContent.style.display = isVisible ? "none" : "block";
+//   dropdownTitle.classList.toggle("open");
+// });
